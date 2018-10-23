3284b95286f839080091b56b8fa9ed306f1012ae
zhangjun <136841189@qq.com>
2018-10-23 13:43:16 +0800

    init

--- src/main/java/com/config/MyWebSecurityConfigurerAdapter.java
+++ src/main/java/com/config/MyWebSecurityConfigurerAdapter.java
@@ -1,6 +1,5 @@
 package com.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -9,10 +8,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.MessageDigestPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import sun.security.util.Password;
 
 @Configuration
 public class MyWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
